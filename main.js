55b0d3406017343d35f28e0a3a271b242a7f0e85
MaazRaza009 <infomaazraza@gmail.com>
2024-04-04 00:52:14 +0500

    data type covered

---
+++ main.js
@@ -0,0 +1,21 @@
+//let name1 = "Varaiblr by var"
+//console.log(name1)
+//name1 = "ubaid"
+//console.log(name1)
+//var name1 = "Variable by var"
+//var name1 = "Maaz Raza"
+//console.log(name1)
+//const name1 = "Variable by const"
+// name1 = "Ubaid"
+//console.log(name1)
+var name1 = "Ubaid"; //inferring type
+var name2 = "Ubaid"; //strongly typed
+//name2 = 1000   errors
+var num1 = 522; //infering type
+var num2 = 1000; //strongly typed
+var news = false; //infering type
+var news1 = true; //strongly typed
+var anyData = 1000;
+anyData = "Hello";
+anyData = true;
+console.log(anyData);
---
+++ main.ts
@@ -0,0 +1,29 @@
+//let name1 = "Varaiblr by var"
+//console.log(name1)
+//name1 = "ubaid"
+//console.log(name1)
+
+//var name1 = "Variable by var"
+//var name1 = "Maaz Raza"
+//console.log(name1)
+
+
+//const name1 = "Variable by const"
+// name1 = "Ubaid"
+//console.log(name1)
+
+let name1 = "Ubaid"  //inferring type
+let name2: string = "Ubaid"  //strongly typed
+//name2 = 1000   errors
+
+let num1 = 522   //infering type
+let num2: number = 1000  //strongly typed
+
+let news = false  //infering type
+let news1: boolean = true  //strongly typed
+
+let anyData:any = 1000  //not recommended
+console.log(anyData)
+anyData = "Hello"
+anyData = true
+console.log(anyData)
